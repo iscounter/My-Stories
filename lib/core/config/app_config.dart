b90da469ca3d6e4426bc572342ae20168be61f7a
kids-coder <kaytlynn@puuntong.com>
2026-09-11 13:32:38 +0000

    fix error

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,5 +1,6 @@
 class AppConfig {
   static const String apiBaseUrl =
       'https://zerotech.alwaysdata.net/app/index.php';
+  static const String apiPrefix = '';
   static const String defaultAdminEmail = 'admin@zerotech.alwaysdata.net';
 }
